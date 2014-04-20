437b28acb23390ae451a8207d7fdf53895492f6a
Xindong Ding <dxd.spirits@gmail.com>
2014-04-20 15:10:25 +0800

    filter site for love messages

--- lovemessages/apiviews.py
+++ lovemessages/apiviews.py
@@ -7,3 +7,10 @@ class MessageViewSet(ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
     paginate_by = 10
+    
+    def get_queryset(self):
+        queryset = self.queryset
+        params = self.request.QUERY_PARAMS
+        if params and 'site' in params:
+            queryset = queryset.filter(site = params['site'])
+        return queryset
